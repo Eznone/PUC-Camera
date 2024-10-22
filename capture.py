f25c04c57601f1949a7780e874381f7152c73ec0
Enzo <enzotresmediano@gmail.com>
2024-10-22 08:55:50 -0300

    Project will focus now more on YOLO

--- capture.py
+++ capture.py
@@ -38,80 +38,13 @@ def processFrame(frame, gamma = 1.0):
     frame = adjust_gamma(frame, gamma)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame = cv2.GaussianBlur(frame, (5,5), 0)
-    _, frame = cv2.threshold(frame, 50, 230, cv2.THRESH_BINARY_INV)
+    _, frame = cv2.threshold(frame, 80, 190, cv2.THRESH_BINARY_INV)
     frame = cv2.morphologyEx(frame, cv2.MORPH_OPEN, kernel)
-    return frame
+    edged = cv2.Canny(frame, 111, 230)
+    edged = cv2.dilate(edged,kernel,iterations=1)
 
 
-def createSharpFrame(frame):
-    kernel = np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]], dtype=np.float32)
-
-    imgLaplacian = cv2.filter2D(frame, cv2.CV_32F, kernel)
-    sharp = np.float32(frame)
-    imgResult = sharp - imgLaplacian
-    # convert back to 8bits gray scale
-    imgResult = np.clip(imgResult, 0, 255)
-    imgResult = imgResult.astype('uint8')
-    imgLaplacian = np.clip(imgLaplacian, 0, 255)
-    imgLaplacian = np.uint8(imgLaplacian)
-    #cv.imshow('Laplace Filtered Image', imgLaplacian)
-    #cv2.imshow('New Sharped Image', imgResult)
-
-    return imgResult
-
-def waterShedFrame(frame, opening, imgResult):
-    kernel = np.ones((5,5),np.uint8)
-    sure_bg = cv2.dilate(opening,kernel,iterations=3)
- 
-    # Finding sure foreground area
-    dist = cv2.distanceTransform(opening, cv2.DIST_L2, 3)
-    cv2.normalize(dist, dist, 0, 1.0, cv2.NORM_MINMAX)
-    #cv2.imshow('Distance Transform Image', dist)
-
-
-
-
-    # Step to focus on
-    _, dist = cv2.threshold(dist, 0.3, 1.0, cv2.THRESH_BINARY)
-    dist = cv2.dilate(dist, kernel)
-    #cv2.imshow('Peaks', dist)
-    # Opening to remove a LOT of noise
-    eroded = cv2.erode(dist,kernel,iterations = 1)
-    opening = cv2.dilate(eroded,kernel,iterations = 1)
-
-
-    #cv2.imshow('Opening', opening)
-
-
-
-
-
-    dist_8u = opening.astype('uint8')
-    # Find total markers
-    contours, _ = cv2.findContours(dist_8u, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # Create the marker image for the watershed algorithm
-    markers = np.zeros(dist.shape, dtype=np.int32)
-    # Draw the foreground markers
-    for i in range(len(contours)):
-        cv2.drawContours(markers, contours, i, (i+1), -1)
-
-    # Draw the background marker
-    cv2.circle(markers, (5,5), 3, (255,255,255), -1)
-    markers_8u = (markers * 10).astype('uint8')
-    #cv2.imshow('Markers', markers_8u)
-
-    cv2.watershed(frame, markers)
-    mark = markers.astype('uint8')
-    mark = cv2.bitwise_not(mark)
-    #cv2.imshow('Markers_v2', mark)
-
-    _, mark = cv2.threshold(mark, 50, 230, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-
-    return mark
-
-
-
+    return edged
 
 
 def secondWaterShed(original_frame, threshed_frame):
@@ -129,19 +62,16 @@ def secondWaterShed(original_frame, threshed_frame):
     # Finding unknown region
     sure_fg = np.uint8(sure_fg)
 
-    cv2.imshow("sure fore", sure_fg)
-    cv2.imshow("sure fback", sure_bg)
+    #cv2.imshow("sure fore", sure_fg)
+    #cv2.imshow("sure fback", sure_bg)
 
 
     unknown = cv2.subtract(sure_bg,sure_fg)
 
-    cv2.imshow("unknown", unknown)
+    #cv2.imshow("unknown", unknown)
 
     # Marker labelling
     ret, markers = cv2.connectedComponents(sure_fg)
-    
-    cv2.imshow("markers", markers)
-
 
     # Add one to all labels so that sure background is not 0, but 1
     markers = markers+1
@@ -152,11 +82,31 @@ def secondWaterShed(original_frame, threshed_frame):
     markers = cv2.watershed(original_frame, markers)
     original_frame[markers == -1] = [255,0,0]
 
+    markers_normalized = cv2.normalize(markers, None, 0, 255, cv2.NORM_MINMAX)
+
+    # Convert the normalized markers to 8-bit so that they can be displayed
+    markers_8bit = np.uint8(markers_normalized)
+
+    # Apply a colormap for better visualization
+    # You can use any colormap, here we use 'cv2.COLORMAP_JET' for example
+    colored_markers = cv2.applyColorMap(markers_8bit, cv2.COLORMAP_JET)
+
+    # Display the markers with the colormap
+    # cv2.imshow("Markers with Colormap", colored_markers)
+
     #cv2.imshow("Water2", original_frame)
     return original_frame
 
 
+def canny(frame):
+    kernel = np.ones((3,3),np.uint8)
 
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(frame, 30, 150)
+    edged = cv2.dilate(edged,kernel,iterations=1)
+
+    return edged
 
 
 def drawBoxes(original_frame, threshed_frame):
@@ -184,17 +134,24 @@ while True:
     frame = imutils.resize(frame, width=1200)
 
     # Createing sharp image
-    sharp_frame = createSharpFrame(frame)
 
     # Processing frame
     threshed_frame = processFrame(frame, 0.5)
-    watershed_frame = waterShedFrame(frame.copy(), threshed_frame, sharp_frame)
     second_watershed_frame = secondWaterShed(frame.copy(), threshed_frame)
+    canny_frame = canny(frame.copy())
 
     # boxes = returnBoxes(frame)
 
     frame2 = frame.copy()
-    frame2 = drawBoxes(frame2, watershed_frame)
+    frame3 = frame.copy()
+    frame2 = drawBoxes(frame2, canny_frame)
+    frame3 = drawBoxes(frame3, threshed_frame)
+
+
+    # Display the frame
+    cv2.imshow('Thresh Frame', threshed_frame)
+    #cv2.imshow('WaterShed Frame', watershed_frame)
+    cv2.imshow("Canny Frame", canny_frame)
 
     # boxes = returnBoxes(frame)
     # for box in boxes:
@@ -202,10 +159,9 @@ while True:
     #     cv2.rectangle(frame3, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 
-    # Display the frame
-    #cv2.imshow('Thresh Frame', threshed_frame)
-    #cv2.imshow('WaterShed Frame', watershed_frame)
-    cv2.imshow('Boxes', frame2)
+    
+    cv2.imshow('Boxes2', frame2)
+    cv2.imshow('Boxes3', frame3)
 
     # Key event listener to end video replay
     if cv2.waitKey(delay) & 0xFF == ord('q'):
